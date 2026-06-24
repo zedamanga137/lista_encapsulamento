adafc2a0143bfa3a39e444990d1d2a84e159f58e
Juliano Romero Santos <2025324281@aluno.poa.ifrs.edu.br>
2026-06-24 15:15:09 -0300

    atividade em prdução

--- q2.py
+++ q2.py
@@ -1,18 +1,107 @@
-class batata:
-    def __init__(self, tipo):
-        self.tipo = tipo
-        self.__peso = 5
+import datetime
+
+class Clinica:
+    def __init__(self, endereco):
+        self._nome = "Pata Certa"
+        self.__endereco = endereco
+        self.__atend = []
+        self.__tabela_preco = {
+    "1": {
+        "categoria": "Consulta",
+        "servico": "Consulta Geral de Rotina",
+        "preco": 150.00,
+        "tempo_estimado_min": 30
+    },
+    "2": {
+        "categoria": "Consulta",
+        "servico": "Consulta de Emergência",
+        "preco": 280.00,
+        "tempo_estimado_min": 45
+    },
+    "3": {
+        "categoria": "Vacinação",
+        "servico": "Vacina Múltipla V10",
+        "preco": 130.00,
+        "tempo_estimado_min": 15
+    },
+    "4": {
+        "categoria": "Vacinação",
+        "servico": "Vacina Antirrábica",
+        "preco": 90.00,
+        "tempo_estimado_min": 15
+    },
+    "5": {
+        "categoria": "Exame",
+        "servico": "Hemograma Completo",
+        "preco": 85.00,
+        "tempo_estimado_min": 720
+    },
+    "6": {
+        "categoria": "Exame",
+        "servico": "Ultrassonografia Abdominal",
+        "preco": 220.00,
+        "tempo_estimado_min": 40
+    },
+    "7": {
+        "categoria": "Procedimento",
+        "servico": "Castração Canino",
+        "preco": 450.00,
+        "tempo_estimado_min": 120
+    },
+    "8": {
+        "categoria": "Procedimento",
+        "servico": "Castração Felino",
+        "preco": 350.00,
+        "tempo_estimado_min": 90
+    },
+    "9": {
+        "categoria": "Estética",
+        "servico": "Banho e Tosa",
+        "preco": 80.00,
+        "tempo_estimado_min": 60
+    }
+}
+
+    def nova_consulta(self, servico, pet):
+        data = datetime.date.today()
 
     @property
-    def peso(self):
-        return self.__peso
+    def endereco(self):
+        return self.__endereco
     
-    @peso.setter
-    def peso(self, nv):
-        self.__peso = nv
+    @property
+    def tabela_preco(self):
+        return self.__tabela_preco
+        
+        
 
-b1 = batata("inglêsa")
-print(b1.tipo)
-print(b1.peso)
-b1.peso = 8
-print(b1.peso)
+class Pet:
+    def __init__(self,nome, especie, raca, peso, idade):
+        self.__info = []
+        self.__info.append(nome)
+        self.__info.append(especie)
+        self.__info.append(raca)
+        
+        if peso <= 0 or idade <= 0:
+            raise ValueError("digite valores acima de de 0")
+        
+        else:
+            self.__info.append(peso)
+            self.__info.append(idade)
+
+        @property
+        def info (self):
+            return {'nome': info[0],
+                    'espécie': info[1],
+                    'raça': info[2]
+                    }
+        
+        def levar_clinica(self, servico,motivo, agendar = None):
+            if agendar == None:
+                clinica.nova_consulta(servico, motivo, agendar)
+            else:
+
+                clinica.nova_consulta(servico, motivo)
+
+clinica = Clinica("Rua dos Bobos, 0 - Bairro Imaginário - São Paulo - SP - CEP: 00000-000")
+print(clinica.endereco)
